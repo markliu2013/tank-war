303aa7f13c04187dafd1e51b3d047b2a1f768d69
Mark <markliu2013@gmail.com>
2014-08-28 21:42:10 +0800

    prototype for grid

--- js/Grid.js
+++ js/Grid.js
@@ -7,19 +7,19 @@
 function Grid(rowsNum, colsNum) {
 	this.rowsNum = rowsNum;
 	this.colsNum = colsNum;
-	this.draw = function() {
-		var gridHTML = '';
-		for(var i=0; i<this.rowsNum; i++) {
-			gridHTML += '<div class="row clearfix">';
-			for(var j=0; j<this.colsNum; j++) {
-				gridHTML += '<div class="col">';
-				gridHTML += '</div>';
-			}
+}
+Grid.prototype.draw = function() {
+	var gridHTML = '';
+	for(var i=0; i<this.rowsNum; i++) {
+		gridHTML += '<div class="row clearfix">';
+		for(var j=0; j<this.colsNum; j++) {
+			gridHTML += '<div class="col">';
 			gridHTML += '</div>';
 		}
-		jQuery('#tank-grid').html(gridHTML);
+		gridHTML += '</div>';
 	}
-	this.init = function() {
-		this.draw();
-	}
+	jQuery('#tank-grid').html(gridHTML);
+}
+Grid.prototype.init = function() {
+	this.draw();
 }

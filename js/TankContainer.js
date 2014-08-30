17fdfba66be1b7eb61d0c45789f837333b752be6
markliu2013 <markliu2013@gmail.com>
2014-08-30 14:21:01 +0800

    Prototype optimize for TankContainer Class

--- js/TankContainer.js
+++ js/TankContainer.js
@@ -1,17 +1,17 @@
 function TankContainer() {
 	this.tanks = [];
-	this.checkWhichTank = function(coordinate) {
-		var tanksLength = this.tanks.length;
-		for (var i=0; i<tanksLength; i++) {
-			var tank = this.tanks[i];
-			var tankArr = tank.getDataArr();
-			for (var j=0; j<tankArr.length; j++) {
-				if (checkEqual(coordinate, tankArr[j])) {
-					this.tanks.splice(i,1);
-					return tank;
-				}
+}
+TankContainer.prototype.checkWhichTank = function(coordinate) {
+	var tanksLength = this.tanks.length;
+	for (var i=0; i<tanksLength; i++) {
+		var tank = this.tanks[i];
+		var tankArr = tank.getDataArr();
+		for (var j=0; j<tankArr.length; j++) {
+			if (checkEqual(coordinate, tankArr[j])) {
+				this.tanks.splice(i,1);
+				return tank;
 			}
 		}
-		return null;
 	}
+	return null;
 }
